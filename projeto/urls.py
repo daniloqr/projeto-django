f211731afb96136b07890dce3e5a6ce2860bea19
Thalita <thalitasa90@gmail.com>
2023-01-20 17:00:27 -0300

    página sobre url

---
+++ projeto/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path
+
+
+def my_view(request):
+    return HttpResponse('Hello World')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('sobre/', my_view),
+
+]
